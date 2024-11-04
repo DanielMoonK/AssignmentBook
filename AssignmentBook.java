80f5d2865f7bbed863ad7b2c375eecd4b67fc8f2
DanielMoonK <danielk213@nycstudents.net>
2024-11-04 17:34:52 +0000

    Do Next Method

--- AssignmentBook.java
+++ AssignmentBook.java
@@ -3,6 +3,7 @@ public class AssignmentBook
     //Schedules are formatted so that rows will be periods, columns will be intervals of minutes available
     //Assume that each interval of minutes cycles from "available" and "not available", with the first interval being "not available"
     private int[][] schedule;
+    private int sumOfMin = 0;
 
     public AssignmentBook(int[][] userSchedule){
         schedule = userSchedule;
@@ -16,10 +17,33 @@ public class AssignmentBook
             }
             s += "\n";
         }
-        return
+        return s;
     }
 
     private boolean isMinuteFree(int period, int minute){
-        while(minute < )
+        // for(int i=0; i<schedule[period].length; i++){
+        //     sumOfMin += schedule[period][i];
+        // }
+        if(minute < schedule[period][0]){
+            return false;
+        }
+        for(int i=0; i<schedule[period].length-1; i++){
+            sumOfMin += schedule[period][i];
+            // System.out.println(sumOfMin);
+            // System.out.println(schedule[period][1]);
+            if((i % 2 == 0) && (minute > sumOfMin) && (minute < (sumOfMin + schedule[period][i]))){
+                return true;
+            }
+            if((i % 2 == 1) && (minute > sumOfMin) && (minute < (sumOfMin + schedule[period][i]))){
+                return false;
+            }
+        }
+        return false;
+    }
+
+
+
+    public void testIsMinFree(int period, int minute){
+        System.out.println(isMinuteFree(period, minute));
     }
 }
--- Main.java
+++ Main.java
@@ -2,6 +2,17 @@ public class Main
 {
     public static void main(String[] args)
     {
-
+        int[][] dayOne = {
+            {10, 5, 15, 15, 10, 5},
+            {15, 15, 20, 10},
+            {9, 11, 30, 5, 5},
+            {15, 5, 5, 15, 20},
+            {30, 20, 10},
+            {10, 20, 30},
+            {10, 5, 5, 15, 25},
+            {5, 15, 15, 10, 5, 10}
+        };
+        AssignmentBook dayEight = new AssignmentBook(dayOne);
+        dayEight.testIsMinFree(1, 20);
     }
 }
