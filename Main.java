92efe6b023e185990384c680d4adcb2eb8e51386
DanielMoonK <danielk213@nycstudents.net>
2024-11-08 17:01:47 +0000

    Final

--- Main.java
+++ Main.java
@@ -22,15 +22,22 @@ public class Main
         for (int i=5; i<30; i++) scheduleTwo[3][i] = true;
         for (int i=44; i<60; i++) scheduleTwo[3][i] = true;
 
-        // int period = 2;
-        // while (period < 5){
-        //     System.out.println("Period" + period);
-        //     b.printPeriod(period);
-        //     period++;
-        // }
+        int period = 2;
+        while (period < 5){
+            System.out.println("Period" + period);
+            b.printPeriod(period);
+            period++;
+        }
 
-        b.printPeriod(4);
         System.out.println(b.makeAppointment(2, 4, 22));
-        b.printPeriod(4);
+        System.out.println(b.makeAppointment(3, 4, 3));
+        System.out.println(b.makeAppointment(2, 4, 30));
+
+        period = 2;
+        while (period < 5){
+            System.out.println("Period" + period);
+            b.printPeriod(period);
+            period++;
+        }
     }
 }
